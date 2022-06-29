6a259df1d8f5d4f5b9b9e3149d30eb281e651ca0
Atilla MUMCULAR <atillamumcular@hotmail.com>
2022-06-30 00:59:38 +0300

    - Added GridCustomizationController which makes headers' fonts bold

---
+++ StarterCRM19.Module.Win/Controllers/GridCustomizationController.cs
@@ -0,0 +1,30 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Win.Editors;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarterCRM19.Module.Win.Controllers
+{
+    public class GridCustomizationController : ViewController<ListView> //we want to effect listviews thus inherited from ViewController<ListView>
+    {
+        public GridCustomizationController()
+        {
+
+        }
+
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            GridListEditor editor = View.Editor as GridListEditor;
+            if(editor != null)
+            {
+                Font font = editor.GridView.Appearance.HeaderPanel.Font;
+                editor.GridView.Appearance.HeaderPanel.Font = new Font(font, FontStyle.Bold);
+            }
+        }
+    }
+}
